11700cf78d0f6e413009cb1e8f961f275db3b7aa
Pedro Luna <neko.hitokori@gmail.com>
2016-11-27 23:20:14 -0600

    prueba de "shops.index"

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -8,6 +8,37 @@ class ShopsController extends Controller
 {
     public function index()
     {
-
+        return response()->json([
+            'page' => [],
+            'shops' => [
+                [
+                    'id' => 1,
+                    'name' => 'Shop testing 1',
+                    'image' => '',
+                    'score' => 4.8,
+                    'description' => '',
+                    'price' => 2,
+                    'location' => [0, 0]
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Shop testing 2',
+                    'image' => '',
+                    'score' => 4.8,
+                    'description' => '',
+                    'price' => 2,
+                    'location' => [0, 0]
+                ],
+                [
+                    'id' => 1,
+                    'name' => 'Shop testing 1',
+                    'image' => '',
+                    'score' => 4.8,
+                    'description' => '',
+                    'price' => 2,
+                    'location' => [0, 0]
+                ]
+            ]
+        ]);
     }
 }
